820b96de0906653b3e71c26d0830d6f26a8b11d6
Leonel Dias <leonel.j.dias@gmail.com>
2020-10-05 21:44:16 +0100

    init DigitalTwinDemoApp to test @triedeti/threedigitaltwin

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <World />
+  </div>
+</template>
+
+<script>
+import World from "./components/World.vue";
+
+export default {
+  name: "App",
+  components: {
+    World,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/World.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <canvas id="world" width="100%" height="100%"></canvas>
+  </v-container>
+</template>
+
+<script>
+import { ThreeDigitalTwin } from "@triedeti/threedigitaltwin";
+
+export default {
+  name: "World",
+  created() {
+    var threedigitaltwin = new ThreeDigitalTwin(this.models, this.textures);
+    var canvas = document.getElementById("world");
+    threedigitaltwin.init(canvas);
+  },
+};
+</script>
